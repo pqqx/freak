6fadc5db22e45af498dc12c4bcc660caa51d436b
Zeorle2 <yutani.gg2@gmail.com>
2025-10-31 18:17:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function gen(length, string = "01") {
-  return Array.from(crypto.getRandomValues(new Uint8Array(length))).map((n) => string[n % string.length]).join('')
-}
-while(true) {
-  document.title = gen(64);
-}
+for(;;)document.title=Math.random()
