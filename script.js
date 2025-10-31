6247729a750dd82afe48530ecfa3f4690db8f0bd
Zeorle2 <yutani.gg2@gmail.com>
2025-10-31 18:12:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function generateRandomString(length, string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
-  return Array.from(crypto.getRandomValues(new Uint8Array(length))).map((n) => string[n % string.length]).join('')
-}
-while(true) {
-  document.title = generateRandomString(64);
-}
+for(;;)document.title=Math.random()
